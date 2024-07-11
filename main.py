c795d8e3d779280ab031694917590d6f9b6675fa
Gokuldas <118219335+GokuldasCMP@users.noreply.github.com>
2024-07-11 12:33:17 +0530

    keras updated again

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
-import tensorflow as tf
-import tf_keras as keras 
+import tf_keras as tf
 import google.generativeai as palm
 from dotenv import load_dotenv
 import os
